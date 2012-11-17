c20a1dc903a4e13b4d9380ec7c521090d2c8593b
C-A de Salaberry <cadesalaberry@yahoo.com>
2012-11-16 21:38:32 -0500

    YOOO.

---
+++ src/Timer.java
@@ -0,0 +1,22 @@
+
+public class Timer {
+	
+	int timer = 0;
+	
+	/**
+	 * Returns the time.
+	 * @return currentTime;
+	 */
+	public int getTime(){
+		return timer;
+	}
+	
+	/**
+	 * Increment the Timer by one.
+	 * 
+	 */
+	public void incrementTime(){
+		timer++;
+	}
+	
+}
---
+++ src/strategies/Strategy.java
@@ -0,0 +1,7 @@
+package strategies;
+
+public interface Strategy {
+	
+	public int getSlowRunningAverage();
+	public int getFastRunnindAverage();
+}
